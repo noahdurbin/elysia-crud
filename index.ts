a6a1ed17118d5c1944e60437718c157733ba5ec9
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-12-20 11:36:36 -0700

    feat: added routes for book crud actions

--- index.ts
+++ index.ts
@@ -1,1 +1,55 @@
-console.log("Hello via Bun!");
+import { Elysia, t } from 'elysia';
+import { html } from '@elysiajs/html'
+import { BooksDatabase } from './db.js';
+
+new Elysia()
+  .use(html())
+  .decorate('db', new BooksDatabase())
+  .get("/", () => Bun.file("index.html").text())
+  .get("/script.js", () => Bun.file("script.js").text())
+  .get("/books", ({ db }) => db.getBooks())
+  .post(
+    "/books",
+    async ({ db, body }) => {
+      console.log(body)
+      const id = (await db.addBook(body)).id
+      console.log(id)
+      return { success: true, id };
+    },
+    {
+      schema: {
+        body: t.Object({
+          name: t.String(),
+          author: t.String(),
+        }),
+      },
+    }
+  )
+  .put(
+    "/books/:id",
+    ({ db, params, body }) => {
+      try {
+        db.updateBook(parseInt(params.id), body) 
+        return { success: true };
+      } catch (e) {
+        return { success: false };
+      }
+    },
+    {
+      schema: {
+        body: t.Object({
+          name: t.String(),
+          author: t.String(),
+        }),
+      },
+    }
+  )
+  .delete("/books/:id", ({ db, params }) => {
+    try {
+      db.deleteBook(parseInt(params.id))
+      return { success: true };
+    } catch (e) {
+      return { success: false };
+    }
+  })
+  .listen(3000);
