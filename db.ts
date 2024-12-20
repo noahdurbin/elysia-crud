c091f6462da772c57fbdc2391fa6c5ce17043efb
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-12-20 11:41:45 -0700

    feat: updated the database

--- db.ts
+++ db.ts
@@ -1,40 +1,47 @@
 import { Database } from 'bun:sqlite';
 
 export interface Book {
-  id?: number;
-  name: string;
-  author: string;
+    id?: number;
+    name: string;
+    author: string;
 }
 
 export class BooksDatabase {
-  private db: Database;
+    private db: Database;
 
-  constructor() {
-    this.db = new Database('books.db');
-    // Initialize the Database
-    this.init()
-        .then(() => console.log('Database initialized'))
-        .catch(console.error);
-  }
+    constructor() {
+        this.db = new Database('books.db');
+        // Initialize the database
+        this.init()
+            .then(() => console.log('Database initialized'))
+            .catch(console.error);
+    }
 
-  // get all the books
-  async getBooks() {
-    return this.db.query('SELECT * FROM books').all();
-  }
+    // Get all books
+    async getBooks() {
+        return this.db.query('SELECT * FROM books').all();
+    }
 
-  // Add a book
-  async addBook(book: Book) {
-    // q: Get id type safely
-    return this.db.qquery(`INSERT INTO books (name, author) VALUES (?,?) RETURNING id`).get(book.name, book.author) as Book;
-  }
+    // Add a book
+    async addBook(book: Book) {
+        // q: Get id type safely 
+        return this.db.query(`INSERT INTO books (name, author) VALUES (?, ?) RETURNING id`).get(book.name, book.author) as Book;
+    }
 
-  //delete a book
-  async deleteBook(id: number) {
-    return this.db.run(`DELETE FROM books WHERE id = ${id}`)
-  }
+    // Update a book
+    async updateBook(id: number, book: Book) {
+        return this.db.run(`UPDATE books SET name = '${book.name}', author = '${book.author}' WHERE id = ${id}`)
+    }
 
-  // initialize the Database
-  async init() {
-    return this.db.run('CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT');
-  }
+    // Delete a book
+    async deleteBook(id: number) {
+        return this.db.run(`DELETE FROM books WHERE id = ${id}`)
+    }
+
+    // Initialize the database
+    async init() {
+        return this.db.run('CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, author TEXT)');
+    }
 }
+
+
